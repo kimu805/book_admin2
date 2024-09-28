5eb64ad82faaeb8dfa414a8f48dabf8596cc5825
kimu805 <kimukimuok805@gmail.com>
2024-09-28 15:56:24 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-#   Character.create(name: "Luke", movie: movies.first)
 (1..5).each do |i|
   Publisher.create(name: "publisher #{i}", address: "東京都渋谷")
   Book.create(name: "Book #{i}", published_on: Time.parse("20240928").ago(i.months), price: (i * 1000), publisher_id: i)
