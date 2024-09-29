d72124979c61b3b51693d96ee0c04e73ad5cd803
kimu805 <kimukimuok805@gmail.com>
2024-09-29 18:36:02 +0900

    create User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  resources :publishers do
-    resources :books
-    member do
-      get "detail"
-    end
-    collection do
-      get "search"
-    end
-  end
+  get "/books/:id" => "books#show"
+  delete "/books/:id" => "books#destroy"
+  resources :publishers
 end
