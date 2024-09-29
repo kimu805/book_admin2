7b309bc69950bf72e231640b293f5887e654ea9b
kimu805 <kimukimuok805@gmail.com>
2024-09-29 13:06:51 +0900

    add enum

--- app/models/book.rb
+++ app/models/book.rb
@@ -37,4 +37,11 @@ class Book < ApplicationRecord
     price >= 5000
   end
 
+  # Enum
+  enum sales_status: {
+    reservation: 0, # 予約受付
+    now_on_sale: 1, # 発売中
+    end_of_print: 2, # 販売終了
+  }
+
 end
