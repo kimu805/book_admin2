565f1137a73e117f0bd6b9242b77588f30f5b289
kimu805 <kimukimuok805@gmail.com>
2024-09-29 18:40:22 +0900

    set up routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'profiles/show'
-  get 'profiles/edit'
-  get 'profiles/update'
   get "/books/:id" => "books#show"
   delete "/books/:id" => "books#destroy"
   resources :publishers
+  resource :profile, only: [:show, :edit, :update]
 end
