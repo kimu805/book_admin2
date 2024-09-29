12ffc248abee2feaa835d98527494cb976cc1e22
kimu805 <kimukimuok805@gmail.com>
2024-09-29 13:07:18 +0900

    Update book.rb

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
-  # scope :costly, -> { where("price > ?", 3000)}
-  # scope :written_about, ->(theme) { where("name like ?", "%#{theme}%")}
-  # scope :find_price, ->(price) { find_by(price: price)}
+  scope :costly, -> { where("price > ?", 3000)}
+  scope :written_about, ->(theme) { where("name like ?", "%#{theme}%")}
+  scope :find_price, ->(price) { find_by(price: price)}
 
   # Association
   belongs_to :publisher
